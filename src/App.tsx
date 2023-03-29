0cc368970a19f4db296625e00305c76c75694e7e
PabloChavez03 <pablooscarchavez@gmail.com>
2023-03-29 19:55:16 -0300

    need to add useEffect

--- src/App.tsx
+++ src/App.tsx
@@ -1,14 +1,43 @@
-import styles from './App.module.css'
-import Dictionary from './components/Dictionary'
-import Header from './components/Header'
+// import { useEffect, useState } from "react";
+import styles from "./App.module.css";
+import Dictionary from "./components/Dictionary/Dictionary";
+import Header from "./components/Header/Header";
+import Loader from "./components/Loader/Loader";
+import results from "./mocks/WithResults.json";
+import { adapterApi } from "./utils/adapterApi";
 
-function App (): JSX.Element {
+// const API_DICTIONARY = "https://api.dictionaryapi.dev/api/v2/entries/en/";
+
+// const URL_DEMO = API_DICTIONARY + "bomb";
+
+function App(): JSX.Element {
+  // const [result, setResult] = useState(null);
+  // const [loading, setLoading] = useState(false);
+
+  // useEffect(() => {
+  //   fetch(URL_DEMO)
+  //     .then((res) => res.json())
+  //     .then((json) => {
+  //       setLoading(true);
+  //       setResult(json);
+  //     })
+  //     .catch((err) => {
+  //       console.error(err);
+  //     })
+  //     .finally(() => {
+  //       setLoading(false);
+  //     });
+  // }, []);
+
+  const wordSearch = adapterApi(results[0]);
+
   return (
     <main className={styles.main}>
       <Header />
-      <Dictionary />
+      {/* {loading ? <Loader /> : } */}
+      <Dictionary wordSearch={wordSearch} />
     </main>
-  )
+  );
 }
 
-export default App
+export default App;
