b117d9da6bbbd58b8c8ead1f627d088ebc242e1e
PabloChavez03 <pablooscarchavez@gmail.com>
2023-03-29 19:57:05 -0300

    add adapter

---
+++ src/utils/adapterApi.ts
@@ -0,0 +1,24 @@
+import { type WordProperties } from '../types/types'
+
+export function adapterApi (results: WordProperties): WordProperties {
+  return {
+    word: results.word,
+    phonetic: results.phonetic,
+    phonetics: results.phonetics.map((phonetic) => ({
+      text: phonetic.text,
+      audio: phonetic.audio,
+      sourceUrl: phonetic.sourceUrl,
+      license: phonetic.license
+    })),
+    meanings: results.meanings.map((meaning) => ({
+      partOfSpeech: meaning.partOfSpeech,
+      definitions: meaning.definitions.map((definition) => ({
+        definition: definition.definition,
+        example: definition.example,
+        synonyms: definition.synonyms
+      }))
+    })),
+    license: results.license,
+    sourceUrls: results.sourceUrls
+  }
+}
