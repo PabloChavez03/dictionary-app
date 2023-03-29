152a65577144c5261d619729402a568b95a25a2d
PabloChavez03 <pablooscarchavez@gmail.com>
2023-03-28 22:28:33 -0300

    add divider horizontal

---
+++ src/assets/icons/DividerHorizontal.tsx
@@ -0,0 +1,9 @@
+import styles from './DividerHorizontal.module.css'
+
+const DividerHorizontal = (): JSX.Element => {
+  return (
+    <div className={styles.divider}/>
+  )
+}
+
+export default DividerHorizontal
