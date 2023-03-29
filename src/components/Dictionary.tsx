8c9744a3e1a585dd54ee3349f623507977778e71
PabloChavez03 <pablooscarchavez@gmail.com>
2023-03-28 22:28:45 -0300

    add dictionary and styles

--- src/components/Dictionary.tsx
+++ src/components/Dictionary.tsx
@@ -1,3 +1,4 @@
+import DividerHorizontal from '../assets/icons/DividerHorizontal'
 import Play from '../assets/icons/Play'
 import styles from './Dictionary.module.css'
 
@@ -11,10 +12,33 @@ const Dictionary = (): JSX.Element => {
             <span>/`ki:bc:d/</span>
           </div>
           <div className={styles.playButtonContainer}>
-            <button className={styles.playButton}><Play size={32}/></button>
+            <button className={styles.playButton}>
+              <Play size={32} />
+            </button>
           </div>
         </div>
-        <div className={styles.dictionaryBody}>body</div>
+        <div className={styles.dictionaryDivider}>
+          <strong>noun</strong>
+          <DividerHorizontal />
+          <div />
+        </div>
+        <div className={styles.dictionaryBody}>
+          <div className={styles.meaning}>
+            <h2>Meaning</h2>
+            <ul>
+              <li>
+                <span>(etc.) A set of keys used to operate a typewriter, computer etc.</span>
+              </li>
+              <li>
+                <span>Lorem ipsum dolor sit amet consectetur adipisicing elit. Eveniet neque vitae asperiores commodi aperiam illum, ipsum, temporibus unde sed libero, molestias assumenda ducimus deleniti totam minus. Animi facere consequatur amet.</span>
+              </li>
+              <li><span>lorem ipsum dolor sit amet consectetur adipisicing elit. Eveniet neque vitae asperiores commodi aperiam illum, ipsum, temporibus unde sed libero, molestias assumend</span></li>
+            </ul>
+          </div>
+          <div className={styles.synonyms}>
+            <h2>Synonyms</h2>
+          </div>
+        </div>
       </main>
       <footer className={styles.footer}></footer>
     </section>
