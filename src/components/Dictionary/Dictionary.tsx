ae640ebc728fbe44ab120166d9c28312f2b01eaa
PabloChavez03 <pablooscarchavez@gmail.com>
2023-03-30 15:02:08 -0300

    add loader and fix bugs

--- src/components/Dictionary/Dictionary.tsx
+++ src/components/Dictionary/Dictionary.tsx
@@ -4,24 +4,41 @@ import { type WordProperties } from '../../types/types'
 import DictionaryHeader from '../DictionaryHeader/DictionaryHeader'
 import DictionaryBody from '../DictionaryBody/DictionaryBody'
 import DictionaryFooter from '../DictionaryFooter/DictionaryFooter'
+import Loader from '../Loader/Loader'
+import Error404 from '../Error/Error404'
 
 interface Props {
   wordSearch: WordProperties
+  loading: boolean
 }
 
-const Dictionary = ({ wordSearch }: Props): JSX.Element => {
+const Dictionary = ({ wordSearch, loading }: Props): JSX.Element => {
   const { word, phonetics, meanings, sourceUrls } = wordSearch
-  const phoneticsComplete = phonetics.filter(el => el.text !== undefined && el.audio !== undefined)[0]
-  const meaningsSlice = meanings.slice(0, 2)
+  const phoneticsComplete = phonetics?.filter(
+    (el) => el.text !== undefined && el.audio !== undefined && el.audio !== ''
+  )[0]
+  const meaningsSlice = meanings?.slice(0, 2)
 
   return (
     <section className={styles.section}>
-      <main className={styles.main}>
-        <DictionaryHeader word={word} phonetics={phoneticsComplete}/>
-        <DictionaryBody meanings={meaningsSlice}/>
-      </main>
-      <DictionaryDivider />
-      <DictionaryFooter sources={sourceUrls}/>
+      {loading
+        ? (
+        <Loader />
+          )
+        : !Array.isArray(wordSearch)
+            ? (
+        <>
+          <main className={styles.main}>
+            <DictionaryHeader word={word} phonetics={phoneticsComplete} />
+            <DictionaryBody meanings={meaningsSlice} />
+          </main>
+          <DictionaryDivider />
+          <DictionaryFooter sources={sourceUrls} />
+        </>
+              )
+            : (
+        <Error404 />
+              )}
     </section>
   )
 }
