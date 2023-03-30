25851aba1dadb78071b731f3d662a0a6a654db48
PabloChavez03 <pablooscarchavez@gmail.com>
2023-03-30 15:01:56 -0300

    add hook useWord

--- src/App.tsx
+++ src/App.tsx
@@ -1,43 +1,22 @@
-// import { useEffect, useState } from "react";
-import styles from "./App.module.css";
-import Dictionary from "./components/Dictionary/Dictionary";
-import Header from "./components/Header/Header";
-import Loader from "./components/Loader/Loader";
-import results from "./mocks/WithResults.json";
-import { adapterApi } from "./utils/adapterApi";
+import { useState } from 'react'
+import styles from './App.module.css'
+import Dictionary from './components/Dictionary/Dictionary'
+import Header from './components/Header/Header'
+// import results from './mocks/WithResults.json'
+import { useWord } from './hooks/useWord'
 
-// const API_DICTIONARY = "https://api.dictionaryapi.dev/api/v2/entries/en/";
+function App (): JSX.Element {
+  const [wordBySearch, setWordBySearch] = useState('hello')
+  const [loading, setLoading] = useState(true)
 
-// const URL_DEMO = API_DICTIONARY + "bomb";
-
-function App(): JSX.Element {
-  // const [result, setResult] = useState(null);
-  // const [loading, setLoading] = useState(false);
-
-  // useEffect(() => {
-  //   fetch(URL_DEMO)
-  //     .then((res) => res.json())
-  //     .then((json) => {
-  //       setLoading(true);
-  //       setResult(json);
-  //     })
-  //     .catch((err) => {
-  //       console.error(err);
-  //     })
-  //     .finally(() => {
-  //       setLoading(false);
-  //     });
-  // }, []);
-
-  const wordSearch = adapterApi(results[0]);
+  const { wordSearch } = useWord({ wordBySearch, setLoading })
 
   return (
     <main className={styles.main}>
-      <Header />
-      {/* {loading ? <Loader /> : } */}
-      <Dictionary wordSearch={wordSearch} />
+      <Header searchWord={setWordBySearch}/>
+      <Dictionary wordSearch={wordSearch} loading={loading}/>
     </main>
-  );
+  )
 }
 
-export default App;
+export default App
