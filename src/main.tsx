29a09eb74d8f66fa64aa132a6c85f6d5806888c6
PabloChavez03 <pablooscarchavez@gmail.com>
2023-04-03 16:07:44 -0300

    add font

--- src/main.tsx
+++ src/main.tsx
@@ -3,11 +3,14 @@ import ReactDOM from 'react-dom/client'
 import App from './App'
 import { ThemeProvider } from './contexts/ThemeContext'
 import './index.css'
+import { FontProvider } from './contexts/FontContext'
 
 ReactDOM.createRoot(document.getElementById('root') as HTMLElement).render(
   <React.StrictMode>
     <ThemeProvider>
-      <App />
+      <FontProvider>
+        <App />
+      </FontProvider>
     </ThemeProvider>
   </React.StrictMode>
 )
