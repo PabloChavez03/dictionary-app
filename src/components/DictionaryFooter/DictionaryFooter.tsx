9ed7a31e228a71d7d3f6e69cb17fd512f4063b77
PabloChavez03 <pablooscarchavez@gmail.com>
2023-04-04 21:34:58 -0300

    fix bugs and fix not found page

--- src/components/DictionaryFooter/DictionaryFooter.tsx
+++ src/components/DictionaryFooter/DictionaryFooter.tsx
@@ -1,4 +1,5 @@
 import ExternalLink from '../../assets/icons/ExternalLink'
+import { useFontProvider } from '../../contexts/FontContext'
 import { useThemeProvider } from '../../contexts/ThemeContext'
 import { type WordProperties } from '../../types/types'
 import styles from './DictionaryFooter.module.css'
@@ -9,10 +10,11 @@ interface Props {
 
 const DictionaryFooter = ({ sources }: Props): JSX.Element => {
   const { isDark } = useThemeProvider()
+  const { fontFamily } = useFontProvider()
   return (
     <footer className={styles.footer}>
       <span style={{ color: isDark ? '#BFBFBF' : 'rgb(100,100,100)' }}>Source</span>
-      <a href={sources?.[0]}>
+      <a href={sources?.[0]} style={{ fontSize: fontFamily === 'mono' ? 12 : 16 }}>
       {sources?.[0]} <ExternalLink />
       </a>
     </footer>
