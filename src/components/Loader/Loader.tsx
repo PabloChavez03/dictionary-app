6a877f07bdefab7a70bb91891263e9e92ebab5ae
PabloChavez03 <pablooscarchavez@gmail.com>
2023-04-04 21:56:29 -0300

    center loader and styles

--- src/components/Loader/Loader.tsx
+++ src/components/Loader/Loader.tsx
@@ -1,6 +1,10 @@
+import styles from './Loader.module.css'
+
 const Loader = (): JSX.Element => {
   return (
-    <div>Loading...</div>
+    <div className={styles.loader}>
+      <h1>Loading...</h1>
+    </div>
   )
 }
 
