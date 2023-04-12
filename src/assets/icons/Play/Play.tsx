f63466b61ac0898d71d7d14d00c2eacabaaa5dec
PabloChavez03 <pablooscarchavez@gmail.com>
2023-04-11 22:26:56 -0300

    change strokeWidth

--- src/assets/icons/Play/Play.tsx
+++ src/assets/icons/Play/Play.tsx
@@ -7,7 +7,7 @@ const Play = (): JSX.Element => {
       xmlns="http://www.w3.org/2000/svg"
       className={className}
       viewBox="0 0 24 24"
-      strokeWidth="1.5"
+      strokeWidth="2"
       stroke="currentColor"
       fill="none"
       strokeLinecap="round"
